5d1b36dc4354828aafec995ebe2a6a4f44de52af
Shadzzo <97727190+Shadzzo@users.noreply.github.com>
2023-03-09 10:18:35 +0300

    Update Wordle.js

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -25,8 +25,11 @@ export default function Wordle({ solution }) {
   
     return (
       <div>
-        <div>solution - {solution}</div>
-        <div>Current Guess - {currentGuess}</div>
+        {/*
+          Cheats
+          <div>solution - {solution}</div>
+          <div>Current Guess - {currentGuess}</div>   
+        */}
         <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
         <Keypad usedKeys={usedKeys}/>
         {showModal && <Modal isCorrect={isCorrect} turn={turn} solution={solution}/>}
